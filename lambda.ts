388fb900424940ccfb9e812eed3cda13279ff03a
mugtaba-subahi <m_subahi@hotmail.com>
2020-05-19 05:44:16 +0100

    updated pipeline

--- lambda.ts
+++ lambda.ts
@@ -5,7 +5,7 @@ import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
 
 const resolvers = {
   Query: {
-    allPosts: () => 'posts whyx',
+    allPosts: () => 'posts whyxe',
   },
 };
 
