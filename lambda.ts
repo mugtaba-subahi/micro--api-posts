1aa1161aceea4536ea467d944926ae7f7e5a44e0
mugtaba-subahi <m_subahi@hotmail.com>
2020-05-25 06:01:31 +0100

    changed from yarn.lock to package-lock

--- lambda.ts
+++ lambda.ts
@@ -7,7 +7,7 @@ const typeDefs = importSchema('schema.graphql');
 
 const resolvers = {
   Query: {
-    allPosts: () => 'new post',
+    allPosts: () => 'new post - changed from yarn to npm',
   },
 };
 
